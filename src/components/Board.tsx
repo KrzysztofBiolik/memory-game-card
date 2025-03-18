8a315027ec5fb4c5f93d8ec78e49f7583df2808f
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2025-03-18 10:17:00 +0100

    Image generating function implemented

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,22 @@
 import "../styles/game.scss";
+import { useEffect } from "react";
+import { useGameStore } from "../store";
 import Tile from "./Tile";
 
 const Board: React.FC = () => {
-  const tiles = Array.from({ length: 16 }, (_, index) => <Tile key={index} />);
+  const { tiles, initializeGame } = useGameStore();
 
-  return <div className="game-board">{tiles}</div>;
+  useEffect(() => {
+    initializeGame(); // Ładujemy kafelki na start
+  }, []);
+
+  return (
+    <div className="game-board">
+      {tiles.map((tile) => (
+        <Tile key={tile.id} id={tile.id} image={tile.image} />
+      ))}
+    </div>
+  );
 };
 
 export default Board;
